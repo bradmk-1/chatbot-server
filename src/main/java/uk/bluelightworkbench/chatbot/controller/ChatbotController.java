9ad5327d718ed4248451fff03634002aaa0501d1
Bradley Moore <bmoore@dsfire.gov.uk>
2021-11-24 18:42:52 +0000

    Annotate controller to allow UI requests to pass cors checks

--- src/main/java/uk/bluelightworkbench/chatbot/controller/ChatbotController.java
+++ src/main/java/uk/bluelightworkbench/chatbot/controller/ChatbotController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -25,6 +26,7 @@ import uk.bluelightworkbench.chatbot.model.*;
  *
  */
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 
 public class ChatbotController {
